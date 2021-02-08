b8749e613d3711702a1ad640eee8ea2bc524b805
Shubhangi Gaikwad <shubhangig@winchestertechnologies.com>
2021-02-08 21:03:12 +0530

    Initial Commit

---
+++ Skynet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Skynet
+//
+//  Created by Ios development on 08/02/21.
+//  Copyright © 2021 Ios development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
